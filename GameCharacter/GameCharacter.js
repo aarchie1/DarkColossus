add20f93f00807e15ca06c97a26da3387d3a2648
Ledagra <svanluven90@gmail.com>
2023-01-10 15:16:50 -0800

    Character movement
    Character left/right movement, jumping, simple gravity, and boundary limitations.

--- GameCharacter/GameCharacter.js
+++ GameCharacter/GameCharacter.js
@@ -1,5 +1,10 @@
 class GameCharacter {
     #sprite;
+    FLOOR_BOUNDARY = 728;
+    CEILING_BOUNDARY = 40;
+    LEFT_BOUNDARY = 40;
+    RIGHT_BOUNDARY = 984;
+
 
     constructor(theHP, theAttack, theSpeed, theName, theSprite, theCoins, theGame) {
         this.game = theGame;
@@ -9,22 +14,51 @@ class GameCharacter {
         this.name = theName;
         this.sprite = theSprite;
         this.coins = theCoins;
-        this.x = 0;
-        this.y = 0;
+        this.x = 100;
+        this.y = 100;
+        this.moveRight = true;
+        this.moveLeft = true;
+        this.moveUp = true;
+        this.moveDown = true;
     };
 
     update(){
-        if(gameEngine.keys['s'] && !gameEngine.keys['w']) {
-            this.y += 2;
-        } else if(gameEngine.keys['s'] && gameEngine.keys['d'] && !gameEngine.keys['w']) {
-            this.y += 2;
-            this.x += 2;
-        } else if(gameEngine.keys['w'] && !gameEngine.keys['s']) {
-            this.y -= 2;
-        } else if(gameEngine.keys['a'] && !gameEngine.keys['d']) {
+        if(gameEngine.keys['a'] && !gameEngine.keys['d'] && this.moveLeft) {
             this.x -= 2;
-        } else if(gameEngine.keys['d'] && !gameEngine.keys['a']) {
+        } else if(gameEngine.keys['d'] && !gameEngine.keys['a'] && this.moveRight) {
             this.x += 2;
+        }
+
+        if(gameEngine.keys[' '] && !this.moveDown) {
+            this.y = 628;
+        }
+
+        if(this.y < this.FLOOR_BOUNDARY) {
+            this.y += 1;
+        }
+
+        if(this.y == this.FLOOR_BOUNDARY) {
+            this.moveDown = false;
+        } else {
+            this.moveDown = true;
+        }
+        
+        if(this.y == this.CEILING_BOUNDARY) {
+            this.moveUp = false;
+        } else {
+            this.moveUp = true;
+        }
+
+        if(this.x == this.RIGHT_BOUNDARY) {
+            this.moveRight = false;
+        } else {
+            this.moveRight = true;
+        }
+
+        if(this.x == this.LEFT_BOUNDARY) {
+            this.moveLeft = false;
+        } else {
+            this.moveLeft = true;
         }
         
     }
