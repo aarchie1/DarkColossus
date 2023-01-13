22d4982da3d20beb04c8d81f2a0c4ae4437d2f44
Ledagra <svanluven90@gmail.com>
2023-01-12 16:42:42 -0800

    Update GameCharacter.js
    more physics

--- GameCharacter/GameCharacter.js
+++ GameCharacter/GameCharacter.js
@@ -4,12 +4,12 @@ class GameCharacter {
     CEILING_BOUNDARY = 40;
     LEFT_BOUNDARY = 40;
     RIGHT_BOUNDARY = 984;
-    JUMP_SPEED = 600;
+    JUMP_ACC = -600;
     FALL_ACC = 450;
     STOP_FALL = 1575;
-    MIN_WALK = 10;
-    MAX_WALK = 20;
-    RUN_ACC = 10;
+    MIN_WALK = 100;
+    MAX_WALK = 1000;
+    RUN_ACC = 100;
 
 
     constructor(theHP, theAttack, theSpeed, theName, theSprite, theCoins, theGame) {
@@ -37,49 +37,16 @@ class GameCharacter {
     update(){
         const TICK = this.game.clockTick;
 
-        if(this.state < 2) {
-            if(Math.abs(this.velocityX) < this.MIN_WALK) {
-                this.velocityX = 0;
-                this.state = 0;
-                if(gameEngine.keys['a'] && !gameEngine.keys['d']) {
-                    this.left = true;
-                    this.right = false;
-                    this.velocityX -= this.MIN_WALK;
-                }
-                if(gameEngine.keys['d'] && !gameEngine.keys['a']) {
-                    this.right = true;
-                    this.left = false;
-                    this.velocityX += this.MIN_WALK;
-                }
-            } else if(Math.abs(this.velocityX) >= this.MIN_WALK) {
-                if(gameEngine.keys['a'] && !gameEngine.keys['d']) {
-                    this.left = true;
-                    this.right = false;
-                    this.velocityX -= this.RUN_ACC * TICK;;
-                }
-                if(gameEngine.keys['d'] && !gameEngine.keys['a']) {
-                    this.right = true;
-                    this.left = false;
-                    this.velocityX += this.RUN_ACC * TICK;
-                }
-            }
-        }
-
-        this.x += this.velocityX * TICK;
-        this.y += this.velocityY * TICK;
-
-        // if(gameEngine.keys[' '] && this.onGround) {
-            
-        // }
-
         if(!this.onGround) {
             this.y += this.FALL_ACC*TICK;
         }
 
         if(this.y >= this.FLOOR_BOUNDARY) {
+            this.state = 0;
             this.onGround = true;
             this.moveDown = false;
         } else {
+            this.state = 2;
             this.onGround = false;
             this.moveDown = true;
         }
@@ -96,17 +63,64 @@ class GameCharacter {
             this.moveRight = true;
         }
 
-        if(this.x == this.LEFT_BOUNDARY) {
+        if(this.x <= this.LEFT_BOUNDARY) {
             this.moveLeft = false;
         } else {
             this.moveLeft = true;
         }
-        
+
+        if(gameEngine.keys[' '] && this.onGround) {
+            this.velocityY = this.JUMP_ACC;
+        }
+
+        if(this.state < 2) {
+            if(Math.abs(this.velocityX) < this.MIN_WALK) {
+                this.velocityX = 0;
+                this.state = 0;
+                if(gameEngine.keys['a'] && !gameEngine.keys['d'] && this.moveLeft) {
+                    this.state = 1;
+                    this.left = true;
+                    this.right = false;
+                    this.velocityX -= this.MIN_WALK;
+                }
+                if(gameEngine.keys['d'] && !gameEngine.keys['a'] && this.moveRight) {
+                    this.state = 1;
+                    this.right = true;
+                    this.left = false;
+                    this.velocityX += this.MIN_WALK;
+                }
+            } else if(Math.abs(this.velocityX) >= this.MIN_WALK) {
+                if(gameEngine.keys['a'] && !gameEngine.keys['d'] && this.moveLeft) {
+                    this.state = 1;
+                    this.left = true;
+                    this.right = false;
+                    this.velocityX -= this.RUN_ACC * TICK;;
+                }
+                if(gameEngine.keys['d'] && !gameEngine.keys['a'] && this.moveRight) {
+                    this.state = 1;
+                    this.right = true;
+                    this.left = false;
+                    this.velocityX += this.RUN_ACC * TICK;
+                }
+            }
+        }
+
+        if(this.velocityX > this.MAX_WALK) {this.velocityX = this.MAX_WALK};
+
+        this.x += this.velocityX * TICK;
+        this.y += this.velocityY * TICK;
+
+        // if(gameEngine.keys[' '] && this.onGround) {
+            
+        // }        
     }
 
     draw(ctx) {
         ctx.beginPath();
         ctx.arc(this.x,this.y,40,0,2*Math.PI);
         ctx.stroke();
+
+        ctx.font = "50px Arial";
+        ctx.fillText("STATE: " + this.state, 10, 50);
     };
 }
