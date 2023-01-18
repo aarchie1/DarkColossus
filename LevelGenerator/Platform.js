44318440dce6bcb341257d3cb44ff9afc5dada55
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2023-01-18 15:40:02 -0800

    Created Platform class
    Platform class has been created.
    Only needs collisions now.
    
    Created folder/file structure for level gen feature

--- LevelGenerator/Platform.js
+++ LevelGenerator/Platform.js
@@ -1,28 +1,19 @@
 class Platform {
-    constructor(startX, startY, endX, endY, spritsheet) {
+    constructor(startX, startY, endX, endY, sprite) {
         this.startX = startX;
         this.startY = startY;
         this.endX = endX;
         this.endY = endY;
-        this.sprite = spritsheet;
-
-        //Might not be needed but its here
+        this.sprite = sprite;
         this.width = endX - startX;
         this.height = endY - startY;
     }
 
-    draw() {
-        //draw the spritesheet
+    draw(ctx) {
+        ctx.drawImage(this.sprite, this.startX, this.startY, this.width, this.height);
     }
 
     update() {
-        //collision
+        
     }
-
-    //Feel free to delete this
-    // collision(player) {
-    //     if (player.x < this.x + this.width && player.x + player.width > this.x && player.y < this.y + this.height && player.y + player.height > this.y) {
-    //         player.stopmovingforwardorwhateverthebehavioris()
-    //     }
-    // }
 }
