74e46292b4e25b67b64149f11ebd5faa08566fb7
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2023-01-21 16:22:50 -0800

    Worked on LevelFactory

--- DNA/DnaFactory.js
+++ DNA/DnaFactory.js
@@ -1,128 +1,122 @@
+//This method will be used to create a new random DNA 
+function getRandomDNA() {
+    const sigmaAbility = getAbility('sigma');
+    const alphaAbility = getAbility('alpha');
+    const betaAbility = getAbility('beta');
+    const epsilonAbility = getAbility('epsilon');
 
-//DNA Factory creates random DNA or DNA based on parents' DNA
-//DNA Factory follows the singleton and factory pattern
-class DnaFactory {
+    let epsilonAbilityCooldownRarity = (epsilonAbility == null) ? 0 : epsilonAbility.cooldownRarity;
+    let epsilonAbilityEffectRarity = (epsilonAbility == null) ? 0 : epsilonAbility.effectRarity;
+
+    const rarity = calculateDnaRarity(
+            sigmaAbility.cooldownRarity,
+            alphaAbility.cooldownRarity,
+            betaAbility.cooldownRarity,
+            epsilonAbilityCooldownRarity,
+            sigmaAbility.effectRarity,
+            alphaAbility.effectRarity,
+            betaAbility.effectRarity,
+            epsilonAbilityEffectRarity
+        );
+            
+    return new DNA(
+            sigmaAbility,
+            alphaAbility,
+            betaAbility,
+            epsilonAbility,
+            rarity
+        );
+}
+
+//This method will be used to create a new DNA based on the parents' DNA
+//We don't use the AbilityFactory because we are not getting random abilities
+//Pass in two DNA objects
+function getDNAFromParents(parent1, parent2) {
+    //Step 1: Inherit abilities from Parent DNA
+    let childSigmaAbility = getAbilityFromParents(parent1.sigmaAbility, parent2.sigmaAbility);
+    let childAlphaAbility = getAbilityFromParents(parent1.alphaAbility, parent2.alphaAbility);
+    let childBetaAbility = getAbilityFromParents(parent1.betaAbility, parent2.betaAbility);
+    let childEpsilonAbility = getAbility('epsilon');
     
-    //This method will be used to create a new random DNA 
-    static getRandomDNA() {
-        const sigmaAbility = AbilityFactory.getAbility('sigma');
-        const alphaAbility = AbilityFactory.getAbility('alpha');
-        const betaAbility = AbilityFactory.getAbility('beta');
-        const epsilonAbility = AbilityFactory.getAbility('epsilon');
+    //Step 2: Inherit cooldown and effect rarity from Parent DNA
+    childSigmaAbility.cooldownRarity = getRarityFromParents(parent1.sigmaAbility.cooldownRarity,parent2.sigmaAbility.cooldownRarity);
+    childAlphaAbility.cooldownRarity = getRarityFromParents(parent1.alphaAbility.cooldownRarity,parent2.alphaAbility.cooldownRarity);
+    childBetaAbility.cooldownRarity = getRarityFromParents(parent1.betaAbility.cooldownRarity,parent2.betaAbility.cooldownRarity);
+    childSigmaAbility.effectRarity = getRarityFromParents(parent1.sigmaAbility.effectRarity,parent2.sigmaAbility.effectRarity);
+    childAlphaAbility.effectRarity = getRarityFromParents(parent1.alphaAbility.effectRarity,parent2.alphaAbility.effectRarity);
+    childBetaAbility.effectRarity = getRarityFromParents(parent1.betaAbility.effectRarity,parent2.betaAbility.effectRarity);
 
-        let epsilonAbilityCooldownRarity = (epsilonAbility == null) ? 0 : epsilonAbility.cooldownRarity;
-        let epsilonAbilityEffectRarity = (epsilonAbility == null) ? 0 : epsilonAbility.effectRarity;
-    
-        const rarity = this.calculateDnaRarity(
-                sigmaAbility.cooldownRarity,
-                alphaAbility.cooldownRarity,
-                betaAbility.cooldownRarity,
-                epsilonAbilityCooldownRarity,
-                sigmaAbility.effectRarity,
-                alphaAbility.effectRarity,
-                betaAbility.effectRarity,
-                epsilonAbilityEffectRarity
-            );
-                
-        return new DNA(
-                sigmaAbility,
-                alphaAbility,
-                betaAbility,
-                epsilonAbility,
-                rarity
-            );
-    };
+    //Step 3: Update the cooldown and effect of the inherited abilities 
+    //        from the inherited rarities
+    childSigmaAbility.setCooldown();
+    childSigmaAbility.setEffect();
+    childAlphaAbility.setCooldown();
+    childAlphaAbility.setEffect();
+    childBetaAbility.setCooldown();
+    childBetaAbility.setEffect();
 
-    //This method will be used to create a new DNA based on the parents' DNA
-    //We don't use the AbilityFactory because we are not getting random abilities
-    //Pass in two DNA objects
-    static getDNAFromParents(parent1, parent2) {
-        //Step 1: Inherit abilities from Parent DNA
-        let childSigmaAbility = this.getAbilityFromParents(parent1.sigmaAbility, parent2.sigmaAbility);
-        let childAlphaAbility = this.getAbilityFromParents(parent1.alphaAbility, parent2.alphaAbility);
-        let childBetaAbility = this.getAbilityFromParents(parent1.betaAbility, parent2.betaAbility);
-        let childEpsilonAbility = AbilityFactory.getAbility('epsilon');
-        
-        //Step 2: Inherit cooldown and effect rarity from Parent DNA
-        childSigmaAbility.cooldownRarity = this.getRarityFromParents(parent1.sigmaAbility.cooldownRarity,parent2.sigmaAbility.cooldownRarity);
-        childAlphaAbility.cooldownRarity = this.getRarityFromParents(parent1.alphaAbility.cooldownRarity,parent2.alphaAbility.cooldownRarity);
-        childBetaAbility.cooldownRarity = this.getRarityFromParents(parent1.betaAbility.cooldownRarity,parent2.betaAbility.cooldownRarity);
-        childSigmaAbility.effectRarity = this.getRarityFromParents(parent1.sigmaAbility.effectRarity,parent2.sigmaAbility.effectRarity);
-        childAlphaAbility.effectRarity = this.getRarityFromParents(parent1.alphaAbility.effectRarity,parent2.alphaAbility.effectRarity);
-        childBetaAbility.effectRarity = this.getRarityFromParents(parent1.betaAbility.effectRarity,parent2.betaAbility.effectRarity);
+    //Step 4: Calculate the rarity of the child DNA
+    let epsilonAbilityCooldownRarity = (childEpsilonAbility == null) ? 0 : childEpsilonAbility.cooldownRarity;
+    let epsilonAbilityEffectRarity = (childEpsilonAbility == null) ? 0 : childEpsilonAbility.effectRarity;
 
-        //Step 3: Update the cooldown and effect of the inherited abilities 
-        //        from the inherited rarities
-        childSigmaAbility.setCooldown();
-        childSigmaAbility.setEffect();
-        childAlphaAbility.setCooldown();
-        childAlphaAbility.setEffect();
-        childBetaAbility.setCooldown();
-        childBetaAbility.setEffect();
+    const rarity = calculateDnaRarity(
+            childSigmaAbility.cooldownRarity,
+            childAlphaAbility.cooldownRarity,
+            childBetaAbility.cooldownRarity,
+            epsilonAbilityCooldownRarity,
+            childSigmaAbility.effectRarity,
+            childAlphaAbility.effectRarity,
+            childBetaAbility.effectRarity,
+            epsilonAbilityEffectRarity
+        );
 
-        //Step 4: Calculate the rarity of the child DNA
-        let epsilonAbilityCooldownRarity = (childEpsilonAbility == null) ? 0 : childEpsilonAbility.cooldownRarity;
-        let epsilonAbilityEffectRarity = (childEpsilonAbility == null) ? 0 : childEpsilonAbility.effectRarity;
-    
-        const rarity = this.calculateDnaRarity(
-                childSigmaAbility.cooldownRarity,
-                childAlphaAbility.cooldownRarity,
-                childBetaAbility.cooldownRarity,
-                epsilonAbilityCooldownRarity,
-                childSigmaAbility.effectRarity,
-                childAlphaAbility.effectRarity,
-                childBetaAbility.effectRarity,
-                epsilonAbilityEffectRarity
-            );
+    //Step 5: Return the child DNA
+    return new DNA(
+            childSigmaAbility,
+            childAlphaAbility,
+            childBetaAbility,
+            childEpsilonAbility,
+            rarity
+        );
+}
 
-        //Step 5: Return the child DNA
-        return new DNA(
-                childSigmaAbility,
-                childAlphaAbility,
-                childBetaAbility,
-                childEpsilonAbility,
-                rarity
-            );
-    }
+function getAbilityFromParents(parent1, parent2) {
+    let childAblity = parent1.dominant ? parent1 : parent2;
 
-    static getAbilityFromParents(parent1, parent2) {
-        let childAblity = parent1.dominant ? parent1 : parent2;
+    if ( (parent1.dominant && parent2.dominant) || 
+        (!parent1.dominant && !parent2.dominant) ) {
+        let random = Math.round(Math.random());
+        childAblity = random ? parent1 : parent2;
+    } 
 
-        if ( (parent1.dominant && parent2.dominant) || 
-            (!parent1.dominant && !parent2.dominant) ) {
-            let random = Math.round(Math.random());
-            childAblity = random ? parent1 : parent2;
-        } 
+    return childAblity;
+}
 
-        return childAblity;
-    }
+function getRarityFromParents(parent1Rarity, parent2Rarity) {
+    let rarityRange = Math.abs(parent1Rarity - parent2Rarity);
+    let rarity = Math.round(Math.random() * rarityRange) + 
+                    Math.min(parent1Rarity, parent2Rarity);
+    return rarity;
+}
 
-    static getRarityFromParents(parent1Rarity, parent2Rarity) {
-        let rarityRange = Math.abs(parent1Rarity - parent2Rarity);
-        let rarity = Math.round(Math.random() * rarityRange) + 
-                     Math.min(parent1Rarity, parent2Rarity);
-        return rarity;
-    }
-
-    static calculateDnaRarity(sigmaCooldownRarity, 
-                        alphaCooldownRarity, 
-                        betaCooldownRarity, 
-                        epsilonCooldownRarity, 
-                        sigmaEffectRarity, 
-                        alphaEffectRarity, 
-                        betaEffectRarity, 
-                        epsilonEffectRarity) {
-        let numberOfAttributes = (epsilonCooldownRarity == 0) ? 6 : 8;
-        let rarity = 0;
-        rarity += sigmaCooldownRarity;
-        rarity += alphaCooldownRarity;
-        rarity += betaCooldownRarity;
-        rarity += sigmaEffectRarity;
-        rarity += alphaEffectRarity;
-        rarity += betaEffectRarity;
-        rarity += epsilonCooldownRarity;
-        rarity += epsilonEffectRarity;
-        rarity = Math.round(rarity / numberOfAttributes);
-        return rarity;
-    }
+function calculateDnaRarity(sigmaCooldownRarity, 
+                    alphaCooldownRarity, 
+                    betaCooldownRarity, 
+                    epsilonCooldownRarity, 
+                    sigmaEffectRarity, 
+                    alphaEffectRarity, 
+                    betaEffectRarity, 
+                    epsilonEffectRarity) {
+    let numberOfAttributes = (epsilonCooldownRarity == 0) ? 6 : 8;
+    let rarity = 0;
+    rarity += sigmaCooldownRarity;
+    rarity += alphaCooldownRarity;
+    rarity += betaCooldownRarity;
+    rarity += sigmaEffectRarity;
+    rarity += alphaEffectRarity;
+    rarity += betaEffectRarity;
+    rarity += epsilonCooldownRarity;
+    rarity += epsilonEffectRarity;
+    rarity = Math.round(rarity / numberOfAttributes);
+    return rarity;
 }
--- LevelGenerator/LevelFactory.js
+++ LevelGenerator/LevelFactory.js
@@ -1,6 +1,126 @@
+function getLevel(levelNumber) {
+    //PUT SECTIONS HERE
+    let sections = [steppingStonesSection, hordeFightSection, uSection, sSection, verticalSection, dnaPickupSection, bossSection, checkpointSection];
+    let view = {x: 1400, y: 900};
 
-//This class is responsible for creating the level object for LevelManager
-class LevelFactory {
+    const platformSmall = {w: 256, h: 256};
+    const platformTiny = {w: 184, h: 284};
+    const platformLarge = {w: 884, h: 496};
+    const hazardGrowthShort = {w: 256, h: 256};
+    const hazardGrowthTall = {w: 256, h: 512};
+    const portal = {w: 364, h: 364};
+    const reaper = {w: 256, h: 256};
+    const molecule = {w: 256, h: 256};
+    const player = {w: 256, h: 256};
 
-    
+    let level = {
+        levelNumber: levelNumber,
+        platformGround: [{x:0, y:view.y}, {x:view.x, y:view.y}],
+        platformTiny: [],
+        platformSmall: [],
+        platformLarge: [],
+        hazardGrowthShort: [],
+        hazardGrowthTall: [],
+        portal: [],
+        reaper: [],
+        molecule: [],
+        background: [{x:0, y:0}],
+        player: [{x: view.x/2, y: view.y - 256}],
+    }
+
+    let startX = view.x;
+    let startY = view.y;
+
+    //Generate the level environment
+
+    //Number of sections per level increases as the level number increases
+    let numberOfSections = 3 + levelNumber;
+
+    const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+    for(let i = 0; i < numberOfSections; i++) {
+        //Generate a section
+        //pick a random section from the list of sections
+        let section = sections[Math.floor(Math.random() * sections.length)];
+        //call the section function
+        section();
+    }
+
+    //create a function to choose which type of platform to generate
+    function choosePlatformType(tinyOnly = false) {
+        let platform = Math.floor(Math.random() * 3);
+        if (tinyOnly) return platformTiny;
+        switch(platform) {
+            case 0:
+                return platformSmall;
+            case 1:
+                return platformTiny;
+            case 2:
+                return platformLarge;
+        }
+    }
+
+
+    //Authored sections below
+
+    //Creates ascending stairs of platforms with 
+    //va
+    function steppingStonesSection() {
+        let numberOfReapers = randomNumber(1, 5 + levelNumber);
+        let numberOfMolecules = randomNumber(1, 5 + levelNumber);
+
+        for (let i = 0; i < randomNumber(1, 5); i++) {
+            let platformType = choosePlatformType();
+
+            if (platformType === platformSmall)
+                level.platformSmall.push({x: startX, y: startY - platformType.h, moving: (Math.random() < 0.5) ? true : false});
+
+            if (numberOfReapers > 0) {   
+                level.reaper.push({x: startX + platformType.w/2 - reaper.w/2, y: startY - platformType.h - reaper.h});
+                numberOfReapers--;
+            }
+            
+            if (numberOfMolecules > 0) {
+                level.molecule.push({x: startX + platformType.w - molecule.w/2, y: startY - platformType.h - molecule.h});
+                numberOfMolecules--;
+            }
+
+            startX += platformplatformType.w;        
+        }
+    }
+
+    function hordeFightSection() {
+
+    }
+
+    function uSection() {
+
+    }
+
+    function sSection() {
+
+    }
+
+    function verticalSection() {
+
+    }
+
+    function dnaPickupSection() {
+
+    }
+
+    function bossSection() {
+
+    }
+
+    function checkpointSection() {
+
+    }
+
+
+    //Filter level based off level modifier
+    return level;
 }
+
+
+
